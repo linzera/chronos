a8f53baf8543dac93cc796adbd438cda5288d968
linzera <italolino@gmail.com>
2025-05-10 19:25:51 -0300

    First blood

---
+++ src/main/java/br/ucsal/fabrica/chronos/ChronosApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.fabrica.chronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosApplication.class, args);
+	}
+
+}
